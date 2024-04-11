5806b55c1107148b73d3be3f555156ee36a23f4e
Anderson Gaitolini <gaitolini@gmail.com>
2024-04-11 17:44:47 -0300

    test

--- components/empty_sensor/sensor.py
+++ components/empty_sensor/sensor.py
@@ -1,13 +1,13 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import sensor
-from esphome.const import CONF_ID, UNIT_EMPTY, ICON_EMPTY
+from esphome.const import CONF_ID, ICON_DATABASE, ICON_CHEMICAL_WEAPON
 
 empty_sensor_ns = cg.esphome_ns.namespace('empty_sensor')
 
 EmptySensor = empty_sensor_ns.class_('EmptySensor', cg.PollingComponent)
 
-CONFIG_SCHEMA = sensor.sensor_schema(UNIT_EMPTY, ICON_EMPTY, 1).extend({
+CONFIG_SCHEMA = sensor.sensor_schema(ICON_DATABASE, ICON_CHEMICAL_WEAPON, 1).extend({
     cv.GenerateID(): cv.declare_id(EmptySensor)
 }).extend(cv.polling_component_schema('60s'))
 
