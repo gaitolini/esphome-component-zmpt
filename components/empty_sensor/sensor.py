4f0c39d3f9acc9dce6c97a51cf8c497a6372450c
Anderson Gaitolini <gaitolini@gmail.com>
2024-04-11 17:51:45 -0300

    ajustes

--- components/empty_sensor/sensor.py
+++ components/empty_sensor/sensor.py
@@ -7,9 +7,15 @@ empty_sensor_ns = cg.esphome_ns.namespace('empty_sensor')
 
 EmptySensor = empty_sensor_ns.class_('EmptySensor', cg.PollingComponent)
 
-CONFIG_SCHEMA = sensor.sensor_schema(ICON_DATABASE, ICON_CHEMICAL_WEAPON, 1).extend({
+CONFIG_SCHEMA = (
+    sensor.sensor_schema(
+        ICON_DATABASE, 
+        ICON_CHEMICAL_WEAPON, 
+        1)
+    .extend({
     cv.GenerateID(): cv.declare_id(EmptySensor)
 }).extend(cv.polling_component_schema('60s'))
+)
 
 def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
